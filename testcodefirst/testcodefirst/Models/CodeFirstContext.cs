c9dae5c22312a3c4da8869b43238ee8046271108
1164733416 <luyanpeng321@126.com>
2020-11-14 20:37:35 +0800

    add a new Code First case 20201114

---
+++ testcodefirst/testcodefirst/Models/CodeFirstContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace testcodefirst.Models
+{
+    public class CodeFirstContext:DbContext
+    {
+        public CodeFirstContext() : base("name=CodeFirstContext") { }
+        public DbSet<Order> Order { get; set; }
+        public DbSet<OrderDetail> OrderDetail { get; set; }
+
+    }
+}
---
+++ testcodefirst/testcodefirst/Models/OrderDetail.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace testcodefirst.Models
+{
+    public class OrderDetail
+    {
+        [Key]
+        public int OrderDetailId { get; set; }
+        public decimal price { get; set; }
+        public int Count { get; set; }
+        [ForeignKey("Order")]
+        public int OrderId { get; set; }
+        public virtual Order Order { get; set; }
+
+
+    }
+}
